b75db5d4ec24556100b0134fb1b9a8a74b620d77
manasamundru <manasa22bce9544@vitapstudent.ac.in>
2025-12-15 14:59:12 +0530

    add the remaining dtos

---
+++ flightBackend/auth-service/src/main/java/com/flightapp/dto/SignUpRequest.java
@@ -0,0 +1,18 @@
+package com.flightapp.dto;
+
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignUpRequest {
+     private String username;
+     @Email
+     private String email;
+     private String password;
+}

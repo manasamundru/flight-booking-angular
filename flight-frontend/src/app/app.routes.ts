ef3d9640c7c4fb68230a9dfb2944c1a892ca2f85
manasamundru <manasa22bce9544@vitapstudent.ac.in>
2025-12-15 23:55:41 +0530

    udate the local changes

--- flight-frontend/src/app/app.routes.ts
+++ flight-frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login';
+import { RegisterComponent } from './register/register';
+import { SearchFlightsComponent } from './search-flights/search-flights';
+import { authGuard } from './auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'search',
+    component: SearchFlightsComponent,
+    canActivate: [authGuard]
+  }
+];
+
